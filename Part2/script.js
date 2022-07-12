ab2c7128181ddada56c63f65a23a3bb9955a8a3e
Rezwan Atcha <rez_a@outlook.com>
2022-07-11 22:10:29 -0400

    Completed up to lecture 40 (Basic Array Operations). Going to do the challenge next.

--- Part2/script.js
+++ Part2/script.js
@@ -1,33 +1,7 @@
 'use strict';
-// CLASS WORK
-// // LECTURE: Functions Calling Other Functions
-// // 1. Create a function called 'describePopulation'. Use the function type you
-// // like the most. This function takes in two arguments: 'country' and
-// // 'population', and returns a string like this: 'China has 1441 million people,
-// // which is about 18.2% of the world.'
-// // 2. To calculate the percentage, 'describePopulation' call the
-// // 'percentageOfWorld1' you created earlier
-// // 3. Call 'describePopulation' with data for 3 countries of your choice
 
-// //FUNCTIONS CALLING OTHER FUNCTIONS
-
-// // IMPORTANT!: Review this section in the course if you are having a hard time with functions!
-
-// function cutFruitPieces(fruit) {
-//   return fruit * 4;
-// }
-
-// function fruitProcessor(apples, oranges) {
-//   const applePieces = cutFruitPieces(apples);
-//   const orangePieces = cutFruitPieces(oranges);
-
-//   const juice = `Juice with ${applePieces} pieces of apple and ${orangePieces} pieces of oranges`;
-//   return juice;
-// }
-
-// console.log(fruitProcessor(2, 3));
 // HOME WORK
-
+//
 // // LECTURE: Functions
 // // 1. Write a function called 'describeCountry' which takes three parameters:
 // // 'country', 'population' and 'capitalCity'. Based on this input, the
@@ -121,6 +95,150 @@
 // console.log(describePopulation('Canada', 3));
 // console.log(describePopulation('China', 1441));
 // console.log(describePopulation('USA', 332));
+
+// // LECTURE: Functions Calling Other Functions
+// // 1. Create a function called 'describePopulation'. Use the function type you
+// // like the most. This function takes in two arguments: 'country' and
+// // 'population', and returns a string like this: 'China has 1441 million people,
+// // which is about 18.2% of the world.'
+// // 2. To calculate the percentage, 'describePopulation' call the
+// // 'percentageOfWorld1' you created earlier
+// // 3. Call 'describePopulation' with data for 3 countries of your choice
+
+// //FUNCTIONS CALLING OTHER FUNCTIONS
+
+// // IMPORTANT!: Review this section in the course if you are having a hard time with functions!
+
+// function cutFruitPieces(fruit) {
+//   return fruit * 4;
+// }
+
+// function fruitProcessor(apples, oranges) {
+//   const applePieces = cutFruitPieces(apples);
+//   const orangePieces = cutFruitPieces(oranges);
+
+//   const juice = `Juice with ${applePieces} pieces of apple and ${orangePieces} pieces of oranges`;
+//   return juice;
+// }
+
+// console.log(fruitProcessor(2, 3));
+
+// // LECTURE: Introduction to Arrays
+// // 1. Create an array containing 4 population values of 4 countries of your choice.
+// // You may use the values you have been using previously. Store this array into a
+// // variable called 'populations'
+// // 2. Log to the console whether the array has 4 elements or not (true or false)
+// // 3. Create an array called 'percentages' containing the percentages of the
+// // world population for these 4 population values. Use the function
+// // 'percentageOfWorld1' that you created earlier to compute the 4
+// // percentage values
+
+// // The countries population in millions
+// const Canada = 38;
+// const USA = 329;
+// const Afghanistan = 39;
+// const Burma = 54;
+
+// const populations = [Canada, USA, Afghanistan, Burma];
+// console.log(populations.length === 4);
+
+// function percentageOfWorld1(countryPopulation) {
+//   return (countryPopulation / 7900) * 100;
+// }
+
+// const percentages = [
+//   percentageOfWorld1(Canada),
+//   percentageOfWorld1(USA),
+//   percentageOfWorld1(Afghanistan),
+//   percentageOfWorld1(Burma),
+// ];
+
+// console.log(percentages);
+
+// LECTURE: Basic Array Operations (Methods)
+// 1. Create an array containing all the neighbouring countries of a country of your
+// choice. Choose a country which has at least 2 or 3 neighbours. Store the array
+// into a variable called 'neighbours'
+// 2. At some point, a new country called 'Utopia' is created in the neighbourhood of
+// your selected country. So add it to the end of the 'neighbours' array
+// 3. Unfortunately, after some time, the new country is dissolved. So remove it from
+// the end of the array
+// 4. If the 'neighbours' array does not include the country ‘Germany’, log to the
+// console: 'Probably not a central European country :D'
+// 5. Change the name of one of your neighbouring countries. To do that, find the
+// index of the country in the 'neighbours' array, and then use that index to
+// change the array at that index position. For example, you can search for
+// 'Sweden' in the array, and then replace it with 'Republic of Sweden'.
+
+const neighbours = [
+  'Canada',
+  'USA',
+  'Brazil',
+  'Jamaica',
+  'Guyanna',
+  'Trinidad',
+  'Tobago',
+];
+
+console.log(neighbours);
+neighbours.push('Utopia');
+console.log(neighbours);
+
+neighbours.pop();
+console.log(neighbours);
+
+if (!neighbours.includes('Germany')) {
+  console.log('Probably not an American country :D');
+}
+
+console.log(neighbours.indexOf('Brazil'));
+neighbours[2] = 'Argentina'; // OR you can do: neighbours[neighbours.indexOf('Brazil')] = 'Argentina';
+console.log(neighbours);
+// CLASS WORK
+
+// // ARRAYS
+// const y = new Array(1991, 1984, 2008, 2020);
+
+// // To get the last element of the array:
+// const friends = ['Michael', 'Steven', 'Peter'];
+// console.log(friends[friends.length - 1]);
+
+// // Your not friends with Michael, but now your new friend is Jay.
+// friends[0] = 'Jay'; // Even though cannot replace primitive values of const, we can still replace a value inside an array
+// console.log(friends);
+
+// const calcAge = function (birthYeah) {
+//   return 2037 - birthYeah;
+// };
+
+// const years = [1990, 1967, 2002, 2010, 2018];
+
+// console.log(calcAge(years[(1, 3, 4)]));
+
+// Although we can replace a single element, what we can't do is the following:
+// friends = ['bob', 'Alice'] <- As you can see, we can't replace the array's values by adding a new array block.
+
+// // Basic Array Operations (Methods)
+// // Add elements
+// const friends = ['Michael', 'Steven', 'Peter'];
+// friends.push('Jay'); //.push adds new value to the end of the array
+// console.log(friends);
+
+// friends.unshift('John');
+// console.log(friends);
+
+// // Remove elements
+// friends.pop(); // last
+// console.log(friends);
+
+// friends.shift(); // first
+// console.log(friends);
+
+// console.log(friends.indexOf('Steven'));
+// console.log(friends.indexOf('Bob'));
+
+// console.log(friends.includes('Steven'));
+// console.log(friends.includes('Bob'));
 
 // CHALLENGE WORK
 // Coding Challenge #1
