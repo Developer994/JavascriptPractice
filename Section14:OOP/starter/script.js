8d0586eb108f123356238fb6a3b6a06002c8a5cb
Rezwan Atcha <rez_a@outlook.com>
2022-12-10 13:21:38 -0500

    Markers for cycling now shows orange and for running, green.

--- Section14:OOP/starter/script.js
+++ Section14:OOP/starter/script.js
@@ -308,3 +308,49 @@ const jay = Object.create(StudentProto);
 jay.init('Jay', 2010, 'Computer Science');
 jay.introduce();
 jay.calcAge();
+
+// ANOTHER CLASS EXAMPLE
+class Account {
+  constructor(owner, currency, pin) {
+    this.owner = owner;
+    this.currency = currency;
+    this.pin = pin;
+    this.movements = []; // For internal commands, such as an array and language, you do not need to add them as paramenters for the constructor.
+    this.locale = navigator.language;
+
+    console.log(`Thanks for opening an account,${owner}`);
+  }
+
+  // Public interface
+  deposit(val) {
+    this.movements.push(val);
+  }
+
+  withdraw(val) {
+    this.deposit(-val);
+  }
+
+  approvedLoan(val) {
+    return true;
+  }
+
+  requestLoan(val) {
+    if (this.approvedLoan(val)) {
+      this.deposit(val);
+      console.log(`Loan approved`);
+    }
+  }
+}
+
+const acc1 = new Account('Jonas', 'EUR', 1111);
+console.log(acc1);
+
+// acc1.movements.push(250);
+// acc1.movements.push(-140);
+acc1.deposit(250);
+acc1.withdraw(140);
+acc1.requestLoan(1000);
+acc1.approvedLoan(1000);
+
+console.log(acc1);
+console.log(acc1.pin);
